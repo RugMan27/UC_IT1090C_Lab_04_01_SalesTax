fbf7cb701689be71da4c6f7fc1fdc6fbbb78cc74
louis smith <smith9lu@mail.uc.edu>
2025-09-02 09:01:26 -0400

    moved code around

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,15 +5,14 @@ public class Main {
 
         // double declarations
         double itemPrice = 20.3;
-        double salesTax = itemPrice * 0.05;
 
         // operation
+        double salesTax = itemPrice * 0.05;
         double totalPrice = itemPrice + salesTax;
 
         // output
         System.out.println("Item Price is  " + itemPrice);
-        System.out.println("Total Price is " + totalPrice);
         System.out.println("Total Tax is " + salesTax);
-
+        System.out.println("Total Price is " + totalPrice);
     }
 }
